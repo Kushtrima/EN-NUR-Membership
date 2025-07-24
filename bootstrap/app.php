2df295917315c0eabd0fc17400c76b9fb998b7d2
Kushtrima <kushtrim.m.arifi@gmail.com>
2025-07-24 16:44:31 +0200

    Integrate Sentry for error tracking and restrict to production environment

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -3,6 +3,7 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Sentry\Laravel\Integration;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -63,5 +64,5 @@ return Application::configure(basePath: dirname(__DIR__))
                  ->name('log-cleanup');
     })
     ->withExceptions(function (Exceptions $exceptions) {
-        //
+        Integration::handles($exceptions);
     })->create(); 
